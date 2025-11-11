b86dace85a339b99ff057eba16bae37dbc88f392
mayagat2002 <mayagat2002@gmail.com>
2025-11-11 18:29:32 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,4 +1,20 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
-    pi = (-1**n_terms)/(2*n_terms+1)
-    return pi
+    """
+    Approximates the value of pi using the Leibniz formula 
+    (Madhava-Leibniz Series) up to n_terms terms.
+    """
+    # Initialize the sum for pi/4
+    pi_over_4_sum = 0.0
+    
+    # The series uses terms starting from k=0 up to k=n_terms-1
+    for k in range(n_terms):
+        # Calculate the k-th term: (-1)^k / (2k + 1)
+        term = ((-1)**k) / (2 * k + 1)
+        
+        # Add the term to the running sum
+        pi_over_4_sum += term
+    
+    # Multiply the sum by 4 to get the approximation of pi
+    pi_approximation = 4 * pi_over_4_sum
+    
+    return pi_approximation
